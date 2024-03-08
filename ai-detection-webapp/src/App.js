31d8c5592f1071e71cc71fd6a416b2ec21fb83d7
Fran-Cilia <francisco.cilia@gmail.com>
2024-03-07 21:54:01 -0800

    Created Basic UI Components

--- ai-detection-webapp/src/App.js
+++ ai-detection-webapp/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    alert(text); // TODO: make call to handler function that performs API call here
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        className="big-text-box"
+        value={text}
+        onChange={handleTextChange}
+        placeholder="Enter your text here..."
+      />
+      <button className="button" onClick={handleButtonClick}>
+        Click me
+      </button>
     </div>
   );
 }
