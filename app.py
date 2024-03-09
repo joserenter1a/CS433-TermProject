504b0bdf25598b0243d3bc67a630f8468e0d91ea
Jose Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-03-09 15:33:11 -0800

    AI URL Scanning endpoint

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from sys import platform as sys_pf
 from tkinter import filedialog
-from emailrep import EmailRep
 
 
 if sys_pf == 'darwin':
@@ -19,7 +18,7 @@ class App(tk.Tk):
         super().__init__(title)
         # Inherit from tk.Tk
         self.title(title)
-        self.geometry('1200x700')
+        self.geometry('800x600')
         self.resizable(False, False)
         self.configure(background="#9177c7")
         
@@ -32,12 +31,12 @@ class App(tk.Tk):
         self.upload_button.place(x = 190, y = 80, relheight = 0.04)
         # Create text entry widget
         self.text_entry = tk.Text(self)
-        self.text_entry.place(x = 100, y = 110, relheight=.38, relwidth=.25)
+        self.text_entry.place(x = 100, y = 110, relheight=.38, relwidth=.4)
         # Create count button
         self.scan_button = tk.Button(self, text="Scan for AI", command=self.ai_scan)
         self.scan_button.place(x = 100, y = 400, relheight = 0.04)
 
-        self.scan__aiplag_button = tk.Button(self, text="AI URL Scan", command=self.ai_plag_scan)
+        self.scan__aiplag_button = tk.Button(self, text="AI URL Scan", command=self.ai_url_scan)
         self.scan__aiplag_button.place(x = 185, y = 400, relheight = 0.04)
         
         self.scan__aiurl_button = tk.Button(self, text="Email Reputation Scan", command=self.scan_email)
@@ -105,19 +104,17 @@ class App(tk.Tk):
         colors = [ '#50ad50','#cc2530']
         fig, ax = plt.subplots()
         ax.pie(pcts, labels=labels, colors=colors, autopct='%1.1f%%')
+        plt.title("AI Generation Score (Text/File)")
         canvas = FigureCanvasTkAgg(fig, self)
-        canvas.get_tk_widget().place(x=600, y=100, relheight=0.25, relwidth=0.2)
+        canvas.get_tk_widget().place(x=500, y=100, relheight=0.35, relwidth=0.35)
         return canvas.draw()
     
-    def ai_plag_scan(self):
+    def ai_url_scan(self):
 
-        url = "https://api.originality.ai/api/v1/scan/ai-plag"
+        url = "https://api.originality.ai/api/v1/scan/url"
 
         payload = json.dumps({
-        "content": self.text_entry.get("1.0", "end-1c"),
-        "title": "optional title",
-        "aiModelVersion": "1",
-        "storeScan": "false"
+        "url": self.text_entry.get("1.0", "end-1c")
         })
         headers = {
         'X-OAI-API-KEY': 'xodrg2zq5987kfcsjwimlhtn1by6u3vp',
@@ -130,11 +127,21 @@ class App(tk.Tk):
         dict_string = response.text
         score = 0
         parsed_dict = self.parse_dict_string(dict_string)
-
         if parsed_dict:
-            score = (parsed_dict["plagiarism"])
-        total_pct = score["total_text_score"]
-        print(response.text)
+            score = (parsed_dict["score"])
+        original_pct = round(score["original"] * 100, 2)
+        ai_pct = round(score["ai"] * 100, 2)
+        pcts = [original_pct, ai_pct]
+        # self.word_count_label.config(text=f'AI Generation Score: Original: {original_pct}% AI: {ai_pct} %')
+        labels = [ "Original", "Ai"]
+        colors = [ '#50ad50','#cc2530']
+        fig, ax = plt.subplots()
+        ax.pie(pcts, labels=labels, colors=colors, autopct='%1.1f%%')
+        plt.title("AI Generation Score (URL)")
+        canvas = FigureCanvasTkAgg(fig, self)
+        canvas.get_tk_widget().place(x=500, y=350, relheight=0.35, relwidth=0.35)
+        return canvas.draw()
+    
     def scan_email(self):
         email = self.text_entry.get("1.0", "end-1c")
         url = f"https://emailrep.io/{email}?summary=true"
